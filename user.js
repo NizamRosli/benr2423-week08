ceec71ccbf9cae681eebacb32c2aa56b729f9441
Soo Yew Guan <sooyewguan@Soos-MacBook-Pro.local>
2022-05-10 10:45:09 +0800

    initial commit

--- user.js
+++ user.js
@@ -5,21 +5,35 @@ class User {
 		users = await conn.db("my-database-name").collection("users")
 	}
 
-	static async register(username, password) {
+	/**
+	 * @remarks
+	 * This method is not implemented yet. To register a new user, you need to call this method.
+	 * 
+	 * @param {*} username 
+	 * @param {*} password 
+	 * @param {*} phone 
+	 */
+	static async register(username, password, phone) {
 		// TODO: Check if username exists
-
 		// TODO: Hash password
-
 		// TODO: Save user to database
+		// faker.js
+		// return
 	}
 
 	static async login(username, password) {
 		// TODO: Check if username exists
+		findOne({username: username})
 
 		// TODO: Validate password
+		compared()
+		return false
 
 		// TODO: Return user object
-		return;
+		return user;
+
+		// faker.js
+		return
 	}
 }
 
--- user.test.js
+++ user.test.js
@@ -1,7 +1,7 @@
 const MongoClient = require("mongodb").MongoClient;
 const User = require("./user")
 
-describe("User Account", () => {
+describe("User Account Management", () => {
 	let client;
 	beforeAll(async () => {
 		client = await MongoClient.connect(
@@ -16,27 +16,30 @@ describe("User Account", () => {
 	})
 
 	test("New user registration", async () => {
-		const res = await User.register("test", "test")
+		const res = await User.register("username", "password", "123456")
 		expect().toBe()
 	})
 
 	test("Duplicate username", async () => {
-		const res = await User.register("test", "test")
+		const res = await User.register("username", "1234")
 		expect().toBe()
 	})
 
 	test("User login invalid username", async () => {
 		const res = await User.login("test", "test")
-		expect().toBe()
+		expect(res).toBe("??")
 	})
 
 	test("User login invalid password", async () => {
 		const res = await User.login("test", "test")
-		expect().toBe()
+		expect(res).toBe("??") 
 	})
 
 	test("User login successfully", async () => {
 		const res = await User.login("test", "test")
-		expect(res).toBe(true)
+		expect(res).toBe()
 	})
+
+	test('should run', () => {
+	});
 });
